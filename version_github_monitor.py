195ffa6c0a166c5d992498b0bfe0bf8a3ccc74ba
Sof1ane <43276871+Sof1ane@users.noreply.github.com>
2022-06-05 23:55:21 +0200

    Update version_github_monitor.py

--- version_github_monitor.py
+++ version_github_monitor.py
@@ -8,9 +8,9 @@ import time
 
 starttime = time.time()
 
-bearer_token = os.environ['BEARER_TOKEN']
-user_to_track = os.environ['user_to_track']
-discord_webhook = os.environ['discord_webhook']
+bearer_token = str(os.environ['BEARER_TOKEN'])
+user_to_track = str(os.environ['user_to_track'])
+discord_webhook = str(os.environ['discord_webhook'])
 
 def create_url():
     # Specify the usernames that you want to lookup below
