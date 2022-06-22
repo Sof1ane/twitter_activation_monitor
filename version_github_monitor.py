03fbde2736b9b84b92951ec3a09c7bbe257169b4
Sofiane <sofianeomari.so@gmail.com>
2022-06-22 21:19:00 +0200

    a

--- version_github_monitor.py
+++ version_github_monitor.py
@@ -4,6 +4,9 @@ import json
 import discord
 from discord import Webhook, RequestsWebhookAdapter, Embed
 import time
+from dotenv import load_dotenv
+
+load_dotenv()
 
 bearer_token = str(os.environ['BEARER_TOKEN'])
 user_to_track = str(os.environ['USER_TO_TRACK'])
@@ -21,7 +24,7 @@ def create_url():
     # Specify the usernames that you want to lookup below
     # You can enter up to 100 comma-separated values.
     usernames = "usernames={}".format(user_to_track)
-    user_fields = "user.fields=description,created_at,name,profile_image_url,protected,url,public_metrics,location,url"
+    user_fields = "user.fields=description,created_at,name,profile_image_url,protected,public_metrics,url"
     # User fields are adjustable, options include:
     # created_at, description, entities, id, location, name,
     # pinned_tweet_id, profile_image_url, protected,
@@ -52,28 +55,38 @@ def connect_to_endpoint(url):
     return response
 
 
-def send_embed(name,username,img_url,description,followers_count,following_count,tweet_count,is_protected,creation_date,id, location,url):
+def send_embed(temp):
+    
+    name = temp["data"][0]["name"]
+    username = temp["data"][0]["username"]
+    img_url = temp["data"][0]["profile_image_url"]
+    description = temp["data"][0]["description"]
+    followers_count = temp["data"][0]["public_metrics"]['followers_count']
+    following_count = temp["data"][0]["public_metrics"]['following_count']
+    tweet_count = temp["data"][0]["public_metrics"]['tweet_count']
+    is_protected = temp["data"][0]["protected"]
+    creation_date = temp["data"][0]["created_at"]
+    id = temp["data"][0]["id"]
+    url = temp["data"][0]["url"]
 
     #### Create the initial embed object ####
-    embed = discord.Embed(title="{} is live".format(username), url="https://twitter.com/{}".format(username), color=0x109319)
+    embed = discord.Embed(title="{} is live".format(temp[1]), url="https://twitter.com/{}".format(temp[1]), color=0x109319)
 
     # Add author, thumbnail, fields, and footer to the embed
-    embed.set_author(name="{}".format(name), url="https://twitter.com/{}".format(username), icon_url="{}".format(img_url.replace("normal", "400x400")))
+    embed.set_author(name="{}".format(temp[0]), url="https://twitter.com/{}".format(temp[1]), icon_url="{}".format(temp[2].replace("normal", "400x400")))
 
-    embed.set_thumbnail(url="{}".format(img_url.replace("normal", "400x400")))
+    embed.set_thumbnail(url="{}".format(temp[2].replace("normal", "400x400")))
 
-    embed.add_field(name="Followers", value="{}".format(followers_count))
-    embed.add_field(name="Followings", value="{}".format(following_count))
-    embed.add_field(name="Tweets", value="{}".format(tweet_count))
-    embed.add_field(name="Protected ?", value="{}".format(is_protected))
-    embed.add_field(name="Creation_date", value="{}".format(creation_date))
-    if description:
-        embed.add_field(name="Description", value="{}".format(description))
-    if location: 
-        embed.add_field(name="Localisation", value="{}".format(location))
-    if url:
-        embed.add_field(name="URL", value="{}".format(url))
-    embed.add_field(name="ID", value="{}".format(id))
+    embed.add_field(name="Followers", value="{}".format(temp[4]))
+    embed.add_field(name="Followings", value="{}".format(temp[5]))
+    embed.add_field(name="Tweets", value="{}".format(temp[6]))
+    embed.add_field(name="Protected ?", value="{}".format(temp[7]))
+    embed.add_field(name="Creation_date", value="{}".format(temp[8]))
+    if temp[3]:
+        embed.add_field(name="Description", value="{}".format(temp[3]))
+    if temp[10]:
+        embed.add_field(name="URL", value="{}".format(temp[10]))
+    embed.add_field(name="ID", value="{}".format(temp[9]))
     
     webhook = Webhook.from_url(discord_webhook, adapter=RequestsWebhookAdapter())
     webhook.send(embed = embed)
@@ -85,27 +98,16 @@ def create_json(response):
 
     json_response= response.json()
 
-    name = json_response["data"][0]["name"]
-    username = json_response["data"][0]["username"]
-    img_url = json_response["data"][0]["profile_image_url"]
-    description = json_response["data"][0]["description"]
-    followers_count = json_response["data"][0]["public_metrics"]['followers_count']
-    following_count = json_response["data"][0]["public_metrics"]['following_count']
-    tweet_count = json_response["data"][0]["public_metrics"]['tweet_count']
-    is_protected = json_response["data"][0]["protected"]
-    creation_date = json_response["data"][0]["created_at"]
-    location = json_response["data"][0]["location"]
-    id = json_response["data"][0]["id"]
-    url = json_response["data"][0]["url"]
-    return (name,username,img_url,description,followers_count,following_count,tweet_count,is_protected,creation_date,location,id,url)
+    
+    return (json_response)
 
     
 def harvest_data():
     url = create_url()
     response = connect_to_endpoint(url)
     
-    # embed_1 = create_embed(name,username,img_url,description,followers_count,following_count,tweet_count,is_protected,creation_date,id,location)
-    return response
+    # embed_1 = create_embed(name,username,img_url,description,followers_count,following_count,tweet_count,is_protected,creation_date,id)
+    return response.json()
 
 
 # def main():
@@ -118,6 +120,7 @@ while True:
     second_harvest = harvest_data()
     if second_harvest.text[2:6] == "data":
         if first_harvest.text[2:6] == "data":
-            if second_harvest != first_harvest:
+            if second_harvest.text != first_harvest.text:
                 temp = create_json(second_harvest)
                 send_embed(temp)
+                
